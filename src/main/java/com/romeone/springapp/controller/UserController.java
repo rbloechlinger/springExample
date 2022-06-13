8ec1fd7d4cbf449ce04db09350aff0c00a7f4345
rbloechlinger <rbloechlinger@gmail.com>
2022-06-13 14:20:37 +0200

    Update user

--- src/main/java/com/romeone/springapp/controller/UserController.java
+++ src/main/java/com/romeone/springapp/controller/UserController.java
@@ -3,16 +3,18 @@ package com.romeone.springapp.controller;
 import com.romeone.springapp.model.User;
 import com.romeone.springapp.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.Optional;
 
 @Controller
+@RequestMapping("/api/user")
 public class UserController {
 
     @Autowired
@@ -36,5 +38,16 @@ public class UserController {
         System.out.println(newUser);
 
         return "register_success";
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<HttpStatus> removeEmployee (@PathVariable("id") long id) {
+        Optional<User> optUser = userRepository.findById(id);
+        if (optUser.isPresent()) {
+            userRepository.delete(optUser.get());
+            return new ResponseEntity<HttpStatus>(HttpStatus.ACCEPTED);
+        }
+
+        return new ResponseEntity<HttpStatus>(HttpStatus.NOT_FOUND);
     }
 }
