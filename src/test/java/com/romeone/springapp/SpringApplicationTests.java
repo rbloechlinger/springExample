57ed5acf4c419242b3eee4b6ddf1ed7f4a7c2b09
rbloechlinger <rbloechlinger@gmail.com>
2022-06-13 08:05:18 +0200

    Update SpringApplicationTests.java

--- src/test/java/com/romeone/springapp/SpringApplicationTests.java
+++ src/test/java/com/romeone/springapp/SpringApplicationTests.java
@@ -1,13 +1,19 @@
 package com.romeone.springapp;
 
+import com.romeone.springapp.controller.AuthController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class SpringApplicationTests {
 
+	@Autowired
+	private AuthController auth;
+
 	@Test
 	void contextLoads() {
+		assertThat(auth).isNotNull();
 	}
-
 }
